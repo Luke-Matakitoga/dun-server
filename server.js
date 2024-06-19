3a640c8d34e3eb9f4e683a00b1f5905c12d694ad
Luke Matakitoga <luke.matakitoga@cetsat.com>
2024-06-19 22:04:50 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mysql = require('mysql');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySQL Connected...');
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Dun API! I\'m presuming you shouldn\'t have access... :)');
+});
+
+app.get('/users', (req, res) => {
+  const sql = 'SELECT * FROM dun_users';
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(results);
+  });
+});
+
+app.post('/users/register', (req, res) => {
+    const {email, username, password} = req.body;
+    if (!email || !username || !password) {
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+    bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        const sql = 'INSERT INTO dun_users (email, username, password) VALUES (?, ?, ?)';
+        db.query(sql, [email, username, hash], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.json(results);
+        });
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
