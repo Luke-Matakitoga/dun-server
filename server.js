711d00a8f610d825add85b93924fe3d001f65651
Luke Matakitoga <luke.matakitoga@cetsat.com>
2024-07-07 19:17:19 +0100

    debug

--- server.js
+++ server.js
@@ -81,6 +81,7 @@ app.get('/auth', (req, res) => {
 
       if (isMatch) {
         // check for a key
+        console.log("checking for key...");
         db.query(`SELECT \`key\` FROM AuthenticationTokens WHERE user_id = ? AND ADDDATE(\`generated\`, INTERVAL lifetime DAY) > NOW()`, [results[0].id], (err, existingAuth)=>{
           if(err){
             return res.status(500).json({error:err});
